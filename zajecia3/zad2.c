ed061d1b5e958304de2983759a824ee4498e0ec1
unknown <tomaszhamerla@interia.pl>
2024-03-16 18:19:57 +0100

    added zad2

---
+++ zajecia3/zad2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main(){
+    int size;
+    printf("Podaj wielkosc macierzy");
+    scanf("%d",&size);
+    int **matrix=(int**)malloc(size * sizeof (int *));
+    for (int i = 0; i < size; ++i) {
+        matrix[i]=(int*) malloc(size*sizeof (int));
+        for (int j = 0; j <size ; ++j) {
+            matrix[i][j]=rand()%6;
+        }
+    }
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j <size ; ++j) {
+            printf("%d ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < size; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+    return 0;
+}
