4da19d0155717e4da93dac39daeb6e69a3e48290
unknown <tomaszhamerla@interia.pl>
2024-05-11 13:58:18 +0200

    added zad5

---
+++ zajecia5/zad5.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void remove_by_index(struct Node** head_ref, int index) {
+    if (*head_ref == NULL)
+        return;
+
+    struct Node* temp = *head_ref;
+
+    if (index == 0) {
+        *head_ref = temp->next;
+        free(temp);
+        return;
+    }
+
+    for (int i = 0; temp != NULL && i < index - 1; i++)
+        temp = temp->next;
+
+    if (temp == NULL || temp->next == NULL)
+        return;
+
+    struct Node* next = temp->next->next;
+
+    free(temp->next);
+
+    temp->next = next;
+}
+
+void remove_by_value(struct Node** head_ref, int value) {
+    struct Node* temp = *head_ref;
+    struct Node* prev = NULL;
+
+    if (temp != NULL && temp->data == value) {
+        *head_ref = temp->next;
+        free(temp);
+        return;
+    }
+
+    while (temp != NULL && temp->data != value) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return;
+
+    prev->next = temp->next;
+    free(temp);
+}
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void print_list(struct Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    push(&head, 6);
+    push(&head, 4);
+    push(&head, 3);
+    push(&head, 2);
+    push(&head, 1);
+
+    printf("Lista poczatkowa: ");
+    print_list(head);
+
+    int index_to_remove = 2;
+    remove_by_index(&head, index_to_remove);
+
+    printf("Lista po usunieciu elementu o indeksie %d: ", index_to_remove);
+    print_list(head);
+
+    int value_to_remove = 4;
+    remove_by_value(&head, value_to_remove);
+
+    printf("Lista po usunieciu elementu o wartosci %d: ", value_to_remove);
+    print_list(head);
+
+    return 0;
+}
